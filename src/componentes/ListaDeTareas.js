b80beb8f06c86dd987fe9020c9ef28224a373c8a
Juan Pablo <juanpablocaceres8@gmail.com>
2024-06-05 15:55:04 -0300

    Paleta de colores + funciones para Eliminar y Agregar tareas

--- src/componentes/ListaDeTareas.js
+++ src/componentes/ListaDeTareas.js
@@ -6,16 +6,43 @@ import '../hojas-de-estilo/ListaDeTareas.css'
 function ListaDeTareas(){
     
     const [tareas, setTareas] = useState([]);
+
+    const agregarTarea = tarea => {
+        if (tarea.texto.trim()){
+            tarea.texto = tarea.texto.trim();
+            const tareasActualizadas = [tarea, ...tareas];
+            setTareas(tareasActualizadas);
+        }
+    }
     
+    const eliminarTarea = id => {
+        const tareasActualizadas = tareas.filter(tarea => tarea.id != id);
+        setTareas(tareasActualizadas)
+    }
+
+    const completarTarea = id => {
+        const tareasActualizadas = tareas.map(tarea => {
+            if (tarea.id === id){
+                tarea.completada = !tarea.completada;
+            }
+            return tarea;
+        })
+        setTareas(tareasActualizadas);
+    }
+
     return(
         <>
-            <TareaFormulario />
+            <TareaFormulario onSubmit={agregarTarea}/>
             <div className='tarea-lista-contenedor'>
                 {
                     tareas.map((tarea) => 
-                        <Tarea 
+                        <Tarea
+                            key={tarea.id}
+                            id={tarea.id} 
                             texto={tarea.texto}
+                            completarTarea={completarTarea}
                             completada = {tarea.completada}
+                            eliminarTarea={eliminarTarea}
                         />
                     )
                 }
